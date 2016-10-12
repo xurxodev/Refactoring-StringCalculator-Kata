c21ac3368ffdea0822fd90c7bd6a0fff7260839e
xurxodev <xurxosf@gmail.com>
2016-10-12 12:10:00 +0200

    create simple implementation

--- SimpleImplementations/Java/StringCalculator.java
+++ SimpleImplementations/Java/StringCalculator.java
@@ -1,10 +1,29 @@
 package com.xurxodev.refactoringstringcalculator;
 
-
-
 public class StringCalculator {
+    public int DEFAULT_RESULT = 0;
 
-    public int add(String numbers) {
-        return 0;
+    public int add(String numbers)
+    {
+        if (numbers=="")
+        {
+            return DEFAULT_RESULT;
+        }
+        if (numbers.contains(","))
+        {
+            return handleMultiple(numbers);
+        }
+        return ParseSingle(numbers);
+    }
+
+    private static int ParseSingle(String input)
+    {
+        return Integer.parseInt(input);
+    }
+
+    private int handleMultiple(String input)
+    {
+        String[] numbers = input.split(",");
+        return add(numbers[0]) + add(numbers[1]);
     }
 }
